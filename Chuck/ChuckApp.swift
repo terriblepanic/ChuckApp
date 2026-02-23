91d478e469f6aa75ed195ce98c92064700e3ebfb
Kirill Panichkin <unbearable.panic@gmail.com>
2026-02-24 00:38:57 +0400

    Initial Commit

---
+++ Chuck/ChuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckApp.swift
+//  Chuck
+//
+//  Created by Кирилл Паничкин on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct ChuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
